e6e7734c5014d2e0c879ff0a2153c619f1e97088
Prochal Szymon <34886043+PirchHD@users.noreply.github.com>
2020-08-15 23:03:36 +0200

    SelectionSort/HeapSort

--- Sorting/MyForm.h
+++ Sorting/MyForm.h
@@ -52,6 +52,8 @@ namespace Sorting {
 	private: System::Windows::Forms::RadioButton^ InsertionSort;
 	private: System::Windows::Forms::RadioButton^ QuickSort;
 	private: System::Windows::Forms::RadioButton^ MergeSort;
+	private: System::Windows::Forms::RadioButton^ SelectionSort;
+	private: System::Windows::Forms::RadioButton^ HeapSort;
 
 
 
@@ -93,6 +95,8 @@ namespace Sorting {
 			this->InsertionSort = (gcnew System::Windows::Forms::RadioButton());
 			this->QuickSort = (gcnew System::Windows::Forms::RadioButton());
 			this->MergeSort = (gcnew System::Windows::Forms::RadioButton());
+			this->SelectionSort = (gcnew System::Windows::Forms::RadioButton());
+			this->HeapSort = (gcnew System::Windows::Forms::RadioButton());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
 			this->SuspendLayout();
@@ -108,9 +112,9 @@ namespace Sorting {
 			// 
 			// Start
 			// 
-			this->Start->Location = System::Drawing::Point(1043, 61);
+			this->Start->Location = System::Drawing::Point(1094, 12);
 			this->Start->Name = L"Start";
-			this->Start->Size = System::Drawing::Size(77, 29);
+			this->Start->Size = System::Drawing::Size(69, 20);
 			this->Start->TabIndex = 1;
 			this->Start->Text = L"Start";
 			this->Start->UseVisualStyleBackColor = true;
@@ -118,19 +122,21 @@ namespace Sorting {
 			// 
 			// Sort
 			// 
-			this->Sort->Location = System::Drawing::Point(1043, 198);
+			this->Sort->BackColor = System::Drawing::Color::Lime;
+			this->Sort->ForeColor = System::Drawing::Color::Black;
+			this->Sort->Location = System::Drawing::Point(1094, 64);
 			this->Sort->Name = L"Sort";
-			this->Sort->Size = System::Drawing::Size(77, 29);
+			this->Sort->Size = System::Drawing::Size(69, 32);
 			this->Sort->TabIndex = 2;
 			this->Sort->Text = L"Let\'s Sort";
-			this->Sort->UseVisualStyleBackColor = true;
+			this->Sort->UseVisualStyleBackColor = false;
 			this->Sort->Click += gcnew System::EventHandler(this, &MyForm::Sort_Click);
 			// 
 			// Random
 			// 
-			this->Random->Location = System::Drawing::Point(1043, 150);
+			this->Random->Location = System::Drawing::Point(1094, 38);
 			this->Random->Name = L"Random";
-			this->Random->Size = System::Drawing::Size(74, 26);
+			this->Random->Size = System::Drawing::Size(69, 20);
 			this->Random->TabIndex = 3;
 			this->Random->Text = L"Random";
 			this->Random->UseVisualStyleBackColor = true;
@@ -138,9 +144,9 @@ namespace Sorting {
 			// 
 			// Reset
 			// 
-			this->Reset->Location = System::Drawing::Point(1043, 105);
+			this->Reset->Location = System::Drawing::Point(1006, 38);
 			this->Reset->Name = L"Reset";
-			this->Reset->Size = System::Drawing::Size(77, 29);
+			this->Reset->Size = System::Drawing::Size(77, 20);
 			this->Reset->TabIndex = 5;
 			this->Reset->Text = L"Reset";
 			this->Reset->UseVisualStyleBackColor = true;
@@ -148,11 +154,11 @@ namespace Sorting {
 			// 
 			// numericUpDown1
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(1043, 35);
+			this->numericUpDown1->Location = System::Drawing::Point(1006, 12);
 			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 9999, 0, 0, 0 });
 			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 2, 0, 0, 0 });
 			this->numericUpDown1->Name = L"numericUpDown1";
-			this->numericUpDown1->Size = System::Drawing::Size(99, 20);
+			this->numericUpDown1->Size = System::Drawing::Size(77, 20);
 			this->numericUpDown1->TabIndex = 8;
 			this->numericUpDown1->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 4, 0, 0, 0 });
 			// 
@@ -164,7 +170,7 @@ namespace Sorting {
 			// BubbleSort
 			// 
 			this->BubbleSort->AutoSize = true;
-			this->BubbleSort->Location = System::Drawing::Point(1035, 256);
+			this->BubbleSort->Location = System::Drawing::Point(1006, 108);
 			this->BubbleSort->Name = L"BubbleSort";
 			this->BubbleSort->Size = System::Drawing::Size(77, 17);
 			this->BubbleSort->TabIndex = 9;
@@ -175,7 +181,7 @@ namespace Sorting {
 			// InsertionSort
 			// 
 			this->InsertionSort->AutoSize = true;
-			this->InsertionSort->Location = System::Drawing::Point(1035, 302);
+			this->InsertionSort->Location = System::Drawing::Point(1006, 131);
 			this->InsertionSort->Name = L"InsertionSort";
 			this->InsertionSort->Size = System::Drawing::Size(84, 17);
 			this->InsertionSort->TabIndex = 10;
@@ -186,7 +192,7 @@ namespace Sorting {
 			// QuickSort
 			// 
 			this->QuickSort->AutoSize = true;
-			this->QuickSort->Location = System::Drawing::Point(1035, 279);
+			this->QuickSort->Location = System::Drawing::Point(1089, 108);
 			this->QuickSort->Name = L"QuickSort";
 			this->QuickSort->Size = System::Drawing::Size(72, 17);
 			this->QuickSort->TabIndex = 11;
@@ -197,7 +203,7 @@ namespace Sorting {
 			// MergeSort
 			// 
 			this->MergeSort->AutoSize = true;
-			this->MergeSort->Location = System::Drawing::Point(1035, 325);
+			this->MergeSort->Location = System::Drawing::Point(1087, 131);
 			this->MergeSort->Name = L"MergeSort";
 			this->MergeSort->Size = System::Drawing::Size(74, 17);
 			this->MergeSort->TabIndex = 12;
@@ -205,11 +211,35 @@ namespace Sorting {
 			this->MergeSort->Text = L"MergeSort";
 			this->MergeSort->UseVisualStyleBackColor = true;
 			// 
+			// SelectionSort
+			// 
+			this->SelectionSort->AutoSize = true;
+			this->SelectionSort->Location = System::Drawing::Point(1006, 154);
+			this->SelectionSort->Name = L"SelectionSort";
+			this->SelectionSort->Size = System::Drawing::Size(88, 17);
+			this->SelectionSort->TabIndex = 13;
+			this->SelectionSort->TabStop = true;
+			this->SelectionSort->Text = L"SelectionSort";
+			this->SelectionSort->UseVisualStyleBackColor = true;
+			// 
+			// HeapSort
+			// 
+			this->HeapSort->AutoSize = true;
+			this->HeapSort->Location = System::Drawing::Point(1091, 154);
+			this->HeapSort->Name = L"HeapSort";
+			this->HeapSort->Size = System::Drawing::Size(70, 17);
+			this->HeapSort->TabIndex = 14;
+			this->HeapSort->TabStop = true;
+			this->HeapSort->Text = L"HeapSort";
+			this->HeapSort->UseVisualStyleBackColor = true;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1183, 640);
+			this->Controls->Add(this->HeapSort);
+			this->Controls->Add(this->SelectionSort);
 			this->Controls->Add(this->MergeSort);
 			this->Controls->Add(this->QuickSort);
 			this->Controls->Add(this->InsertionSort);
@@ -230,7 +260,7 @@ namespace Sorting {
 		}
 #pragma endregion
 
-	public: array<System::Windows::Forms::PictureBox^>^ Sektory;
+	private: array<System::Windows::Forms::PictureBox^>^ Sektory;
 
 		  int n = 99999;
 		  int* tab = new int[n];
@@ -243,17 +273,19 @@ namespace Sorting {
 		pictureBox1->Height = MyForm::Height;
 		pictureBox1->Width = MyForm::Width - 200;
 
-		Start->Left = MyForm::Width - 150;
-		Random->Left = MyForm::Width - 150;
-		Sort->Left = MyForm::Width - 150;
-		Reset->Left = MyForm::Width - 150;
+		Start->Left = MyForm::Width - 100;
+		Random->Left = MyForm::Width - 100;
+		Sort->Left = MyForm::Width - 100;
+		Reset->Left = MyForm::Width - 190;
 
-		BubbleSort->Left = MyForm::Width - 150;
-		QuickSort->Left =  MyForm::Width - 150;
-		InsertionSort->Left  = MyForm::Width - 150;
-		MergeSort->Left = MyForm::Width - 150;;
+		BubbleSort->Left = MyForm::Width - 190;
+		QuickSort->Left =  MyForm::Width - 100;
+		InsertionSort->Left  = MyForm::Width - 190;
+		MergeSort->Left = MyForm::Width - 100;
+		SelectionSort->Left = MyForm::Width - 190;
+		HeapSort->Left = MyForm::Width - 100;
 
-		numericUpDown1->Left = MyForm::Width - 150;
+		numericUpDown1->Left = MyForm::Width - 190;
 	}
 
 
@@ -329,14 +361,21 @@ namespace Sorting {
 		if (MergeSort->Checked) {
 			Merge_Sort(0,n-1);
 			Verification();
-			
+		}
+		if (SelectionSort->Checked) {
+			Selection();
+			Verification();
+		}
+		if (HeapSort->Checked) {
+			Heap();
+			Verification();
 		}
 
 	}
 
-		   //=================Sorting Algorithms=============================
+	//================================Sorting Algorithms===============================================
 
-	public: System::Void LooksOk() {
+	private: System::Void LooksOk() {
 		for (int k = 0; k < n; k++) {
 			Sektory[k]->Location = System::Drawing::Point(pictureBox1->Width * (k) / n, pictureBox1->Height - Sektory[k]->Height - 39);
 		}
@@ -347,14 +386,19 @@ namespace Sorting {
 		for (int i = 0; i < n; i++) {
 			Sektory[i]->BackColor = Color::Green;
 			Application::DoEvents();
-			System::Threading::Thread::Sleep(15);
+			System::Threading::Thread::Sleep(10);
 		}
 	}
 
-	public: System::Void swap(int j) {
-		int temp = Sektory[j]->Height;
-		Sektory[j]->Height = Sektory[j + 1]->Height;
-		Sektory[j + 1]->Height = temp;
+	public: System::Void swap(int xp, int yp) {
+		
+		/*int ^temp = xp;
+		xp = yp;
+		yp = temp;*/
+		
+		int temp = Sektory[xp]->Height;
+		Sektory[xp]->Height = Sektory[yp]->Height;
+		Sektory[yp]->Height = temp;
 	   };
 
 	private: System::Void Bubble() {
@@ -369,7 +413,7 @@ namespace Sorting {
 					Sektory[j]->BackColor = Color::Green;
 
 					
-					swap(j);
+					swap(j,j+1);
 					
 					Sektory[j]->Location = System::Drawing::Point(pictureBox1->Width * j / n, pictureBox1->Height - Sektory[j]->Height - 39);
 					
@@ -440,7 +484,7 @@ namespace Sorting {
 			Sektory[j + 1]->Height = key;	
 		}
 
-		LooksOk(); // i have to change this. This is to get around the problem. Aligns the blocks correctly
+		LooksOk(); // i have to change this. This get around the problem. Aligns the blocks correctly
 		
 		Verification();
 
@@ -502,6 +546,64 @@ namespace Sorting {
 		}
 
 	}
+
+	private: System::Void Selection() {
+		int i, j, min_idx;
+		for (i = 0; i < n - 1; i++) {
+			min_idx = i;
+			for (j = i + 1; j < n; j++) 
+				if (Sektory[j]->Height < Sektory[min_idx]->Height) min_idx = j;
+			Sektory[i]->BackColor = Color::Green;
+			swap(min_idx,i);	
+			Sektory[min_idx]->Location = System::Drawing::Point(pictureBox1->Width * min_idx / n, pictureBox1->Height - Sektory[min_idx]->Height - 39);
+			Application::DoEvents();
+			Sektory[i]->Location = System::Drawing::Point(pictureBox1->Width * i / n, pictureBox1->Height - Sektory[i]->Height- 39);
+			Sektory[i]->BackColor = Color::White;
+		}
+	}
+
+
+	private: System::Void heapify(int n, int i) {
+
+		int largest = i; 
+		int l = 2 * i + 1; // left = 2*i + 1 
+		int r = 2 * i + 2; // right = 2*i + 2 
+
+		if (l < n && Sektory[l]->Height > Sektory[largest]->Height)
+			largest = l;
+
+		
+		if (r < n && Sektory[r]->Height > Sektory[largest]->Height)
+			largest = r;
+
+		
+		if (largest != i)
+		{
+			swap(i, largest);
+			Sektory[largest]->Location = System::Drawing::Point(pictureBox1->Width * largest / n, pictureBox1->Height - Sektory[largest]->Height - 39);
+			heapify(n, largest);
+		}
+	
+	}
+
+	private: System::Void Heap() {
+		for (int i = n / 2 - 1; i >= 0; i--) {
+			heapify(n, i);
+		}
+		for (int i = n - 1; i > 0; i--){
+			
+			swap(0, i);
+			Sektory[i]->Location = System::Drawing::Point(pictureBox1->Width * i / n, pictureBox1->Height - Sektory[i]->Height - 39);
+			Application::DoEvents();
+			heapify(i, 0);
+			Sektory[0]->Location = System::Drawing::Point(pictureBox1->Width * 0 / n, pictureBox1->Height - Sektory[0]->Height - 39);
+
+		}
+
+	}
+
+
+
 
 };
 }
